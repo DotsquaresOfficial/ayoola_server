f0e844da821f2ad1c6267724ebe4320a4327425a
ram pandey <ramkrishan.pandey@dotsquares.com>
2025-05-12 19:29:03 +0530

    initial commit

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -12,6 +12,8 @@ router.post('/login', async (req, res, next) => {
     const { email, password } = req.body;
 
     if (!email || !password) {
+
+      
       return res.status(400).json({
         success: false,
         message: 'Please provide email and password'
