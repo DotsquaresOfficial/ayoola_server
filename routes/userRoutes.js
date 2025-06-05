3243ace8267fd2c8a0d47b5376b710c60379b322
Ram Pandey <developerRamPandey@gmail.com>
2025-06-05 19:12:00 +0530

    add protect

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const { getAllUsers } = require('../controllers/userController');
+const protect = require('../middleware/authMiddleware');
 
-router.get('/', getAllUsers);
+router.get('/',protect, getAllUsers);
 
 module.exports = router;
