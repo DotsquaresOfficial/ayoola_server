3c1c100a0fc29a678b7faeaef0b18324465dbc8a
Ram Pandey <developerRamPandey@gmail.com>
2025-06-10 05:09:06 +0530

    fix routes

--- routes/stepsCoinRoute.js
+++ routes/stepsCoinRoute.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { convertPointsToStepCoins, getStepCoinsConversionHistory, getPendingStepCoinRequests, approveStepCoinRequest, bulkApproveStepCoinRequests } = require('../controllers/stepCoinsController');
+const { convertPointsToStepCoins, getStepCoinsConversionHistory, getPendingStepCoinRequests, approveStepCoinRequest, bulkApproveStepCoinRequests, suspectStepCoinRequest, getApprovedStepCoinRequests, getSuspectedStepCoinRequests } = require('../controllers/stepCoinsController');
 const protect = require('../middleware/authMiddleware');
 const router = express.Router();
 
@@ -8,8 +8,8 @@ router.get('/convert-history/:userId', getStepCoinsConversionHistory);
 router.get('/requests',protect, getPendingStepCoinRequests); 
 router.post('/approve/:id',protect, approveStepCoinRequest); 
 router.post('/bulk-approve',protect, bulkApproveStepCoinRequests); 
-router.post('/suspect/:id',protect, approveStepCoinRequest); 
-router.get('/approved',protect, approveStepCoinRequest); 
-router.get('/suspected',protect, approveStepCoinRequest); 
+router.post('/suspect/:id',protect, suspectStepCoinRequest); 
+router.get('/approved',protect, getApprovedStepCoinRequests); 
+router.get('/suspected',protect, getSuspectedStepCoinRequests); 
 
 module.exports = router;
