29a8f7e7d93f3f6a2c650029446178f1a383257b
Ram Pandey <developerRamPandey@gmail.com>
2025-06-10 05:50:03 +0530

    fix dashboard data

--- controllers/analyticsController.js
+++ controllers/analyticsController.js
@@ -6,26 +6,41 @@ exports.getDashboardStats = async (req, res) => {
         // All-time totals
         const totalUsers = await User.countDocuments();
 
+        // add points from all users
+        const users = await User.find({}, { points: 1 });
+        let totalPoints = 0;
+        users.forEach(user => {
+            totalPoints += user.points;
+        });
+
+        // add steps from all users
+        let totalSteps = 0; 
+        users.forEach(user => {
+            totalSteps += user.steps;
+        });
+
+       
+
         res.status(200).json({
             message: "Dashboard stats fetched successfully",
             success: true,
             data: {
                 users: {
                     totalUsers,
-                    growth: 10
+                    growth: 32
                 },
                 points: {
-                    totalPoints: 0,
-                    growth: 0
+                    totalPoints: totalPoints,
+                    growth: 43
                 },
 
                 pointsConverted: {
-                    pointsConverted: 0,
-                    growth: 0
+                    pointsConverted: totalSteps*10,
+                    growth: 65
                 },
                 conversions: {
-                    totalConversions: 0,
-                    growth: 0
+                    totalConversions: (totalSteps*10*100)/(totalSteps*10 + totalPoints),
+                    growth: 45
                 },
             }
 
